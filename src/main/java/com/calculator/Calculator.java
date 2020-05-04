34e14580430982eeb0f7385878247170d3ce90de
Satvik Ramaprasad <satviksr@gmail.com>
2020-05-04 21:46:02 +0530

    CLI

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,12 +1,50 @@
 package com.calculator;
 
+import java.util.Scanner; 
+
 public class Calculator {
 
 	public static void main(String[] args){
-		System.out.println("6 + 3 = " + add(6, 3));
-		System.out.println("6 - 3 = " + subtract(6,3));
-		System.out.println("6 * 3 = " + multiply(6,3));
-		System.out.println("6 / 3 = " + divide(6,3));
+		Scanner in = new Scanner(System.in); 
+
+		System.out.println("Welcome to Calculator v0.1");
+		System.out.println("");
+		while(true) {
+			System.out.println("What operation would you like to do ?");
+			System.out.println("1) Addition");
+			System.out.println("2) Subtraction");
+			System.out.println("3) Multiplication");
+			System.out.println("4) Division");
+			System.out.println("5) Exit");
+
+			System.out.print("Enter option number [1-5]: ");
+			int opt = in.nextInt(); 
+			if(opt < 1 || opt > 5) System.out.println("Option should be between 1 and 5. Try again!");
+			else if(opt == 5)break;
+			else {
+				System.out.print("Enter operand 1: ");
+				int op1 = in.nextInt(); 
+				System.out.print("Enter operand 2: ");
+				int op2 = in.nextInt(); 
+				if(opt == 1) {
+					int result = add(op1, op2);
+					System.out.println(op1 + " + " + op2 + " = " + result);
+				}
+				else if(opt == 2) {
+					int result = subtract(op1, op2);
+					System.out.println(op1 + " - " + op2 + " = " + result);
+				}
+				else if(opt == 3) {
+					long result = multiply(op1, op2);
+					System.out.println(op1 + " x " + op2 + " = " + result);
+				}
+				else if(opt == 4) {
+					double result = divide(op1, op2);
+					System.out.println(op1 + " / " + op2 + " = " + result);
+				}
+			}
+		}
+		
 	}
 
 	public static int add(int a, int b) {
